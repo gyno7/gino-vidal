c87838f789517565e10b5996e8042bba534513b3
gyno7 <gnvdl7@gmail.com>
2025-09-19 11:59:06 +0800

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -296,7 +296,7 @@
             welcomeText: '',
             responseTimeText: '',
             poweredBy: {
-                text: 'Powered by Mad',
+                text: 'Powered by Gino',
                 link: 'https://n8n.partnerlinks.io/m8a94i19zhqq?utm_source=nocodecreative.io'
             }
         },
